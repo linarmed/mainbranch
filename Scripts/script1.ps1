1c99f912a76dc1bf2cc06ac2f7a7f0618c5c0994
developer2 <developer2@company.com>
2018-04-13 08:40:21 -0700

    Update Configuration.xml, script1.ps1, and script2.ps1

---
+++ Scripts/script1.ps1
@@ -0,0 +1,32 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('5', '6','10','11', '12', '13', '14', '15', '16', '17', '20', '21')] 
+    [string]
+    $Number
+)
+
+$TypeCountMap = @{
+    '5' = 9
+    '6' = 9
+    '10' = 9
+    '11' = 9
+    '12' = 9
+    '13' = 9
+    '14' = 9
+    '15' = 9
+    '16' = 9
+    '17' = 9
+    '20' = 5
+    '21' = 5
+}
+
+
+foreach ($Number in $ToCheck)
+{
+    
+    foreach ($i in 0..($TypeCountMap[$Number] - 1))
+    {
+        $Wildcard = "P$Number`T$i`I*"
+        $AddressWildcard = "*@P$Number`T$i`I*"
+    }
+}
---
+++ Scripts/script2.ps1
@@ -0,0 +1,32 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("4","8","16")] 
+    [string]
+    $Size,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $Prefix
+)
+
+$tenantCountMap = @{
+    "16" = 20000, 3500, 4000, 3000, 800, 400, 70, 12, 4
+    "8"  = 166, 50, 18, 3, 1
+    "4"  = 5000, 875, 1000, 750, 200, 100, 17, 3, 1
+}
+$startTime = [DateTime]::Now
+
+$Counts = $CountMap[$Size]
+
+for ($i=0; $i -lt $Counts.Length; $i++)
+{
+    $Count = $Counts[$i]
+    $total += $Count
+}
+
+echo ""
+$endTime = [DateTime]::Now
+echo ("Duration: " + ($endTime - $startTime).ToString())
+echo ("Total: {0}" -f $total)
+echo ""
+
