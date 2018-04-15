dfa6e65cc7d6420692ad9511f2a542f45c457467
developer1 <developer1@company.com>
2018-04-14 17:32:34 -0700

    Revert "Update script2.ps1 - Display Size parameter"
    This reverts commit e5ff7ea63731b869a33cab97ff61e1c795775871.

---
+++ Scripts/script3.ps1
@@ -0,0 +1,39 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('5', '6','10','11', '12', '13', '14', '15', '16', '17', '20', '21')] 
+    [string]
+    $Number
+)
+
+$TypeCountMap = @{
+    '5' = 9
+    '6' = 9
+    '10' = 9
+    '11' = 9
+    '12' = 9
+    '13' = 9
+    '14' = 9
+    '15' = 9
+    '16' = 9
+    '17' = 9
+    '20' = 5
+    '21' = 5
+}
+
+$startTime = [DateTime]::Now
+
+foreach ($Number in $ToCheck)
+{
+    
+    foreach ($i in 0..($TypeCountMap[$Number] - 1))
+    {
+        $Wildcard = "P$Number`T$i`I*"
+        $AddressWildcard += "*@P$Number`T$i`I*"
+    }
+}
+
+echo ""
+$endTime = [DateTime]::Now
+echo ("Duration: " + ($endTime - $startTime).ToString())
+echo ""
+
